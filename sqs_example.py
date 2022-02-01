b0841e30cc115ca40f10a8cd72d74f162d416b7e
EC2 Default User <ec2-user@ip-172-31-84-167.ec2.internal>
2022-02-01 20:07:44 +0000

    Add: create and delete messages from SQS

--- sqs_example.py
+++ sqs_example.py
@@ -1,6 +1,6 @@
 # Load the AWS SDK for Python
 import boto3
-
+import time
 # Load the exceptions for error handling
 from botocore.exceptions import ClientError, ParamValidationError
 
@@ -23,11 +23,80 @@ def create_sqs_queue(sqs_queue_name):
         print("Parameter validation error: %s" % e)
     except ClientError as e:
         print("Client error: %s" % e)
+        
+# Send 50 SQS messages
+# Send 50 SQS messages
+def create_messages(queue_url):
+    # Create 50 messages in batches of 10
+    TempMessages = []
+    for a in range(5):
+        TempEntries = []
+        for b in range(10):
+            tempStr1 = 'This is the content for message ' + str((a*10+b))
+            tempStr2 = 'Message' + str((a*10+b))
+            tempEntry = {
+                'MessageBody': tempStr1,
+                'Id': tempStr2
+            }
+            TempEntries.append(tempEntry)
+        TempMessages.append(TempEntries)
+        # Deliver messages to SQS queue_url
+    for batch in TempMessages:
+        try:
+            data = sqs.send_message_batch(
+                QueueUrl = queue_url,
+                Entries = batch
+                )
+            print(data['Successful'])
+    # An error occurred
+        except ParamValidationError as e:
+            print("Parameter validation error: %s" % e)
+        except ClientError as e:
+            print("Client error: %s" % e)
+
+# Receive SQS messages
+def receive_messages(queue_url):
+    print('Reading messages')
+    while True:
+        try:
+            data = sqs.receive_message(
+                QueueUrl = queue_url,
+                MaxNumberOfMessages = 10,
+                VisibilityTimeout = 60,
+                WaitTimeSeconds = 20
+            )
+        # An error occurred
+        except ParamValidationError as e:
+            print("Parameter validation error: %s" % e)
+        except ClientError as e:
+            print("Client error: %s" % e)
+        # Check if empty receive
+        try:
+            data['Messages']
+        except KeyError:
+            data = None
+        if data is None:
+            print('Queue empty waiting 60s')
+            # Wait for 60 seconds
+            time.sleep(60)
+        else:
+            for message in data['Messages']:
+                print (message)
+                sqs.delete_message(
+                    QueueUrl = queue_url,
+                    ReceiptHandle = message['ReceiptHandle']
+                    )
+                print('Deleted message')
+            # Wait for 1 second
+            time.sleep(1)
 
 # Main program
 def main():
     sqs_queue_url = create_sqs_queue('backspace-lab')
     print('Successfully created SQS queue URL '+ sqs_queue_url )
+    #create_messages(sqs_queue_url)
+    #print('Successfully created messages')
+    receive_messages(sqs_queue_url)
 
 if __name__ == '__main__':
     main()
